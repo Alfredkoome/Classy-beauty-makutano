fb59d3ba27e9c9c803b147b3053b7eb9a8dec7c3
Koome Alfred <koome.26551@students.kyu.ac.ke>
2026-09-19 17:59:55 +0300

    Add express-session to server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const { rateLimit } = require("express-rate-limit");
+const session = require("express-session");
 const app = express();
 
 const db = mysql.createConnection({
